789ae83bc8a8b8f6a4d56007f895589b5b95d6b9
Will <wm.titus2@gmail.com>
2023-11-09 23:56:17 -0500

    Add horizontal controls

--- game025-final-project/Assets/Scripts/PlayerController.cs
+++ game025-final-project/Assets/Scripts/PlayerController.cs
@@ -9,17 +9,30 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     [Tooltip("How high the character jumps")]
     public float jumpStrength;
-    
+
+    // Private variables
+    private Rigidbody2D myRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Horizontal movement
+        if (Input.GetAxis("Horizontal") < 0) // Left
+        {
+            myRigidbody.velocity = new Vector2(-moveSpeed * Time.deltaTime, myRigidbody.velocity.y);
+            Debug.Log("moving left");
+        }
+
+        if (Input.GetAxis("Horizontal") > 0) // Right
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed * Time.deltaTime, myRigidbody.velocity.y);
+            Debug.Log("moving right");
+        }
     }
 }
