70a791077bda26b50a29ead24c8b9d121ab70185
TippyToo <wm.titus2@gmail.com>
2023-11-14 18:40:06 -0500

    Remove unused method

--- game025-final-project/Assets/Scripts/Damage.cs
+++ game025-final-project/Assets/Scripts/Damage.cs
@@ -6,11 +6,6 @@ public class Damage : MonoBehaviour
 {
     [Tooltip("Amount to damage player")]
     public int damageAmount;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
