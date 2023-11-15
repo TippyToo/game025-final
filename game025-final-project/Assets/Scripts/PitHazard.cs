464c5a2dbb587d7f989f251234623f0a58b69682
TippyToo <wm.titus2@gmail.com>
2023-11-14 22:34:06 -0500

    Add pit hazards

--- game025-final-project/Assets/Scripts/PitHazard.cs
+++ game025-final-project/Assets/Scripts/PitHazard.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PitHazard : MonoBehaviour
 {
+    private Vector2 respawnAt;
+    [Tooltip("Amount to damage player when falling in")]
+    public int damageAmount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        respawnAt = transform.Find("Respawn").position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.transform.CompareTag("Player"))
+        {
+            collision.transform.GetComponent<PlayerController>().Damage(damageAmount);
+            collision.transform.position = respawnAt;
+            collision.transform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
     }
 }
