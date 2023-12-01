2d428041d18fbe780bead7d13adcb6e2f5826bec
TippyToo <wm.titus2@gmail.com>
2023-11-30 20:31:35 -0500

    Implement interactable

--- game025-final-project/Assets/Scripts/LevelManager.cs
+++ game025-final-project/Assets/Scripts/LevelManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     public int coins;
+
+    public Text coinDisplay;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,7 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        coinDisplay.text = coins.ToString();
     }
 
     public void addCoins(int count)
